3c297f549136192feb487eeb4cdb96498d20493b
Pranjal Pandit <pranjal.pandit@ibibogroup.com>
2013-07-19 14:37:41 +0530

    Redis Based Queue and DataStore and multiprocessor with different thread for joining processes.

--- queue.py
+++ queue.py
@@ -1,69 +1,71 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
-from retask import Queue as RetaskQueue
-from retask import Task
+import redis
+import uuid
 
+class RedisQueue(object):
 
-class QueueException(Exception):
-    pass
+    """Simple Queue with Redis Backend"""
+    def __init__(self, name, namespace='queue'):
+        """
+        The default connection parameters are: host='localhost', port=6379, db=0
+        """
+        self.redisDB = redis.Redis()
+        self.key = '%s:%s' %(namespace, name)
 
+    def qsize(self):
+        """Return the approximate size of the queue."""
+        return self.redisDB.llen(self.key)
 
-class Queue(object):
-    """
-    A custom Queue API for our workers so that we can plug an play with
-    multiple Queue implementations.
-    """
+    def empty(self):
+        """Return True if the queue is empty, False otherwise."""
+        return self.qsize() == 0
 
-    def __init__(self, name):
-        if isinstance(name, basestring) and name.strip():
-            self.q = RetaskQueue(name)
-            self.q.connect()
-            self.name = name
+    def put(self, item):
+        """Put item into the queue."""
+        uid = str(uuid.uuid4())
+        redis_data = {}
+        redis_data['data'] = item
+        redis_data['taskid'] = uid
+        self.redisDB.rpush(self.key, redis_data)
+        return uid
+
+    def get(self, block=True, timeout=None):
+        """Remove and return an item from the queue. 
+
+        If optional args block is true and timeout is None (the default), block
+        if necessary until an item is available."""
+        if block:
+            item = self.redisDB.brpop(self.key, timeout=timeout)
         else:
-            raise QueueException(
-                "Bad value for 'name'. 'name' has to be a non empty string.")
+            item = self.redisDB.rpop(self.key)
 
-    def get(self, count=1, wait=False):
-        """
-        Get task from Queue.
+        if item:
+            item = item[1]
+        return item
 
-        Args:
-            wait: A boolean deciding whether to retrieve a task
-                  from queue in a blocking fashion or not.
-        Returns:
-            A list of retask.Task instances.
-        """
-        tasks = []
-        for n in range(count or 1):
-            if wait:
-                task = self.q.wait()
-            else:
-                task = self.q.dequeue()
-            tasks.append(task)
-        return tasks
+    def get_nowait(self):
+        """Equivalent to get(False)."""
+        return self.get(False)
 
-    def put(self, data):
-        """
-        Create a task from data and put it to Queue.
 
-        Args:
-            data: A dictionary.
-        """
-        task = Task(data)
-        job = self.q.enqueue(task)
-        return job
+class RedisDataStore():
+    def __init__(self, name, namespace='hash'):
+        """The default connection parameters are: host='localhost', port=6379, db=0"""
+        self.redisDB= redis.Redis()
+        self.key = '%s:%s' %(namespace, name)
 
-    def send(self, task, data):
-        """
-        Update result for a task and send it to the producer.
+    def get(self, map_key):
+        value = self.redisDB.hget(self.key, map_key)
+        return value
+    
+    def put(self, map_key, value):
+        try:
+            self.redisDB.hset(self.key, map_key, value)
+            return True
+        except Exception as e:
+            return False
 
-        Args:
-            task: A retask.Task instance
-            data: A string.
-        """
-        self.q.send(task, data)
-
-    def __unicode__(self):
-         return self.name
+    def delete(self, map_key):
+        self.redisDB.hdel(self.key, map_key)
 
--- worker.py
+++ worker.py
@@ -8,12 +8,12 @@ fastQ worker implementation
 
 """
 
-import __future__ import absolute_import
 from multiprocessing import Pool, Process, Queue
-from .queue import Queue, Task
-from .datastore import DataStore
+from queue import RedisQueue, Task, RedisDataStore
 import time
-import importlib
+import os, sys
+
+PROCESS_MUTEX = '/tmp/process_mutex'
 
 class Worker(object):
     """
@@ -27,73 +27,109 @@ class Worker(object):
         """
         Initialize worker.
         """
-        self.queue = Queue()
-        self.ds = DataStore()
+        self.queue = RedisQueue('TASK_QUEUE')
+        self.ds = RedisDataStore('RESULT_STORE')
+        self.processes = []
         try:
             self.num_cores = multiprocessing.cpu_count()
         except NotImplementedError:
             self.num_cores = 4
 
-    def _get_from_queue(self, items_to_fetch):
+    def _get_from_queue(self):
         """
         Pull data out from queue, this should return instantly 
         with None if queue is empty or task object if data in queue.
         Return : List of tasks.
         """
         # Task instance from queue.task
-        task_list = self.queue.get(items_to_fetch)
-        for task in task_list:
-            assert isinstance(task, Task)
-        return task_list
+        task_dict = self.queue.get()
+        return task_dict
         
-    def _put_to_process(self, task_list):
+    def _put_to_process(self, task_dict):
         """
         Put data to process through multiprocessing.
         """
-        task_length = len(task_list)
-        pool = Pool(task_length)
-        [pool.apply_async(self._exec_func, task) for task in task_list]
+        #Work through Process(), p.start() but process.join() in a thread.
 
+        #task_length = len(task_list)
+        #pool = Pool(task_length)
+        #[pool.apply_async(self._exec_func, task) for task in task_list]
         
-    def _exec_func(self, task):
+        p = Process(target=self._exec_func, args=(task_dict))
+        p.start()
+        self.processes.append(p)
+        
+    def _exec_func(self, task_dict):
         """
         Parallel calls with multiprocess.
         """
-        func = task.data['func']
-        args = task.data['args']
-        kwargs = task.data['kwargs']
-
-        impt = func.split('.')[-1]
+        task_id = task_dict['taskid']
+        exec_data = task_dict['data']
+        export_list = exec_data['export_list']
+        function = exec_data['function']
+        args = exec_data['args']
+        object_instance = exec_data.get('object_instance')
+        file_path = exec_data.get('file_path')
         
-        mod = __import__(('.'.join(func.split('.')[:-1])), fromlist=[impt])
-        clazz = getattr(mod, impt)
-        instance = clazz(kwargs.get('options'))
-        result = instance.func(*args, **kwargs)
+        
+        for export_item in export_list:
+            sys.path.append(export_item)
+        from Flight.QueryEngine.FlightQueryEngineConnection import FlightQueryEngineConnection
+        from Flight.QueryEngine.FlightQueryEngineCursor import FlightQueryEngineCursor
+        conn = FlightQueryEngineConnection()
+        cursor = FlightQueryEngineCursor(conn)
+        result = cursor.processControllerData(args)
 
-        self._store_in_ds(task, result)
+        """
+        
+        import_str = file_str[len(export_list):]
+        import_str = ".".join(import_str.split('/'))
+        import_str = import_str[:-3] if import_str.endswith('.py') else import_str
 
-    def _store_in_ds(self, task, result):
+        #Put object_instance and export_list ans lists
+        'from %s import %s' %(import_str, object_instance)
+        mod = __import__('import_str', fromlist=[object_instance])
+        clazz = getattr(mod, object_instance)
+        conn = FlightQueryEngineConnection()
+        instance = clazz(conn)
+        exec_str = 'result = instance.' + function + args
+        exec(exec_str)
+
+        # from Flight.QueryEngine.FlightQueryEngineCursor import FlightQueryEngineCursor
+        """
+        self._store_in_ds(task_id, result)
+
+    def _store_in_ds(self, task_id, result):
         """
         After completion of process put the result with
         status in datastore.
         """
-        self.queue.send(task, result)
+        self.ds.put(task_id, result)
 
     def run_worker(self):
         """
         Replace this with higher level async mechanism
         to get data from queue, like signals etc.
         """
-        while 1:
-            try:
-                #Should return instantly.
-                task_list = self._get_from_queue(self.num_cores)
-            
-                if task_list:
-                    self._put_to_process(task_list)
-                
-                time.sleep(0.15)
-            except KeyboardInterrupt:
-                break
-            
-        
+        task_dict = self._get_from_queue()
+        self._put_to_process(task_dict)
+
+def join_workers(worker):
+
+    if os.path.exists(PROCESS_MUTEX):
+        return
+        open(PROCESS_MUTEX, 'w').close()
+
+    num_processes = len(worker.processes)
+    processes_started = worker.processes
+    worker.processes = worker.processes[num_processes:]
+    
+    for process_started in processes_started:
+        process_started.join()
+
+    os.remove(PROCESS_MUTEX)
+
+if __name__ == "__main__":
+    worker = Worker()
+    worker.run_worker()
+
